a768232a01cfbd0fe703a101ac0b5644e1a8fd88
gorengordon <goren@gorengordon.com>
2017-07-12 17:05:33 +0300

    added sync to jsonfile

--- tablet_app/tangram_mindset_app.py
+++ tablet_app/tangram_mindset_app.py
@@ -579,8 +579,9 @@ class TangramMindsetApp(App):
         elif STUDY_SITE == 'MIT':
             local_ip = '192.168.1.254'
         KC.start(the_parents=[self, self.interaction.components['robot']], the_ip=local_ip)  # 127.0.0.1
-        KL.start(mode=[DataMode.file, DataMode.communication, DataMode.ros], pathname=self.user_data_dir, the_ip=local_ip)
-
+        # KL.start(mode=[DataMode.file, DataMode.communication, DataMode.ros], pathname=self.user_data_dir, the_ip=local_ip)
+        # KL.start(mode=[], pathname=self.user_data_dir, the_ip=local_ip)
+        KL.start(mode=[DataMode.file], pathname=self.user_data_dir,)
     def on_connection(self):
         KL.log.insert(action=LogAction.data, obj='TangramMindsetApp', comment='start')
 
